59819323337cfb131ba93c3827cdcdd9e1189920
Willson Ngo <wngo@grubhub.com>
2020-06-14 19:12:22 -0400

    add environment variable

--- amplify/backend/function/sworldmediabackendd/src/main/java/com/sworld/Constants.java
+++ amplify/backend/function/sworldmediabackendd/src/main/java/com/sworld/Constants.java
@@ -1,6 +1,6 @@
 package com.sworld;
 
 public class Constants {
-    public static final String USER_MESSAGES_TABLE = "UserMessages";
+    public static final String USER_MESSAGES_TABLE = "UserMessages_" + System.getenv("ENV");
     public static final String AWS_REGION = "us-east-1";
 }
